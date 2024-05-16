bb621a8197a6621a3938cb99ec639946d4426141
Nguyenhoan03 <phephehoan@gmail.com>
2024-05-17 00:19:52 +0700

    mainserver

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const app = express();
-const dotenv = require("dotenv");
-const port = process.env.PORT || 5000
-
-dotenv.config();
-app.listen(port,()=> console.log('hello world'));
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(8000, function () {
+  console.log('Example app listening on port 3000!');
+});
